8e503e2c7e97a9120635a65f933d0e2a8927225f
Ramon Zambrano <170786939+r-alejo-z95@users.noreply.github.com>
2025-02-16 20:59:52 -0500

    finish excersice 1.13

--- part 1/anecdotes/src/App.jsx
+++ part 1/anecdotes/src/App.jsx
@@ -13,6 +13,13 @@ const App = () => {
   ];
 
   const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  const handleVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
 
   const getRandAnec = () => {
     const updatedSelected = Math.floor(Math.random() * anecdotes.length);
@@ -22,6 +29,8 @@ const App = () => {
   return (
     <div>
       <div>{anecdotes[selected]}</div>
+      <p>Votes: {votes[selected]}</p>
+      <button onClick={handleVote}>vote</button>
       <button onClick={getRandAnec}>next anecdote</button>
     </div>
   );
